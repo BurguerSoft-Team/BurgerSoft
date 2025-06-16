1f999f4f2c595ae7f36120e877eb9f1b15c4695a
g-nzaan <asgmshift@gmail.com>
2025-06-16 10:19:55 -0600

    Update datos_cliente.py

--- Captura_DatosCliente_Validaciones/datos_cliente.py
+++ Captura_DatosCliente_Validaciones/datos_cliente.py
@@ -1,5 +1,44 @@
-
-
+# Capturar: nombre, apellido, dirección, color casa, método de pago
+# Validar datos ingresados (que sean texto, no vacíos, etc.)
+# Este programador solo se enfoca en la parte personal del cliente y sus datos de contacto.
 
 def capturar_datos_cliente():
-    print("Logica aqui!")
+    print("")
+    print("---Datos del cliente---")
+    print("")
+
+    while True: 
+        nombre = input("Ingrese su nombre: ")
+        if nombre.strip() == "":
+            print("Este espacio no puede estar vacío")
+        else: 
+            break
+
+    while True:
+        apellido = input("Ingrese su apellido: ")
+        if apellido.strip() == "":
+            print("Este espacio no puede estar vacío")
+        else: 
+            break
+
+    while True:
+        direccion = input("Ingrese la dirección de su casa: ")
+        if direccion.strip() == "":
+            print("Este espacio no puede estar vacío")
+        else: 
+            break
+
+    while True:
+        color_casa = input("Ingrese el color de su casa: ")
+        if color_casa.strip() == "":
+            print("Este espacio no puede estar vacío")
+        else: 
+            break
+
+    while True:
+        metodo_de_pago = input("Escoja su método de pago (Efectivo o Tarjeta): ")
+        if metodo_de_pago == ["Efectivo", "Tarjeta"]:
+            print("Método inválido. Escriba 'Efectivo' o 'Tarjeta'.")
+        else:
+            print(f"Método seleccionado: {metodo_de_pago}")
+            break
