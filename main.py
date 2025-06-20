cdff7a8a1855efe5c520f704b39c88c8e5bb7cd8
Junaxer <juniordelarocha15@hotmail.com>
2025-06-20 10:48:39 -0600

    Cambio de nombre en los modulos y logica del main (#13)

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
+import os
+from modulo_historial.calcular_ganacia import historial
+from modulo_pedido.menu import mostrar_menu
 
 def main():
-    print("Aqui se iniciara el proyecto!")
+    
+    print("=" * 45)
+    print("   *** Bienvenido a TheBurgerClub Granada ***   ")
+    print("=" * 45)
 
-
-
-
+    print("1.Registrar pedido")
+    print("2.Ver historial de ganacias")
+    
+    while True:
+        opcion = input("Opcion: ")
+        if opcion == "":
+            print("Campo vacio!")
+            continue
+        else:
+            opcionEntero = int(opcion)
+            if opcionEntero == 1:
+                os.system('cls')
+                mostrar_menu()
+                break
+            elif opcionEntero == 2:
+                os.system('cls')  
+                historial()
+                break
+            else:
+                print("Opcion no valido")
+                continue
 
 if __name__ == "__main__":
     main()
