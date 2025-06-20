771cd4f62859affb665474fc88bcf7a0514c4164
JuniorDLR <juniordelarocha15@hotmail.com>
2025-06-19 21:05:25 -0600

    logica de guardar archivo de texto e historial

---
+++ Cancelacion_Historial_Reinicio/calcular_ganacia.py
@@ -0,0 +1,61 @@
+
+from gestion_operaciones import verificar_archivo
+from gestion_operaciones import ARCHIVO_VENTAS
+from datetime import datetime
+import json
+
+def historial():
+    print("Historial de ganacias \n")
+    while True:
+        encabezados = ["Ingrese la fecha de inicio (YYYY-MM-DD):", "Ingrese la fecha de fin (YYYY-MM-DD): "]
+        fechas = []
+        for encabezado in encabezados:
+            while True:
+                dato = pedir_fecha(message=encabezado)
+                if dato != "":
+                    fechas.append(dato)
+                    break
+        # Verifica que la fecha de fin no sea menor que la de inicio
+        if fechas[1] < fechas[0]:
+            print("La fecha de fin no puede ser menor que la fecha de inicio. Intenta de nuevo.\n")
+            continue
+        break
+
+    if not verificar_archivo():
+        print("La base de datos esta vacia!")
+    else:
+        filtrar_ganancias(fechas)
+def pedir_fecha(message):
+    while True:
+        fecha_str = input(message)
+        try:
+            fecha = convertir_cadena_a_datetime(fecha_str)
+            return fecha
+        except ValueError:
+            print("Fecha inválida. Usa el formato YYYY-MM-DD.")
+
+
+def filtrar_ganancias(fechas):
+    fecha_inicio = fechas[0]
+    fecha_fin = fechas[1]
+
+    total = 0
+
+    with open(ARCHIVO_VENTAS,"r") as ventas:
+        for linea in ventas:
+            venta = json.loads(linea)
+            db_fecha = convertir_cadena_a_datetime(venta=venta["fecha"])
+
+            if fecha_inicio <= db_fecha <= fecha_fin:
+                for item in venta["pedido"]:
+                    total += item["precio_unitario"] * item["cantidad"]
+    print("El total es: ",total)
+            
+
+
+def convertir_cadena_a_datetime(venta):
+    return datetime.strptime(venta, "%Y-%m-%d").date()
+
+if __name__ == "__main__":
+    historial()
+
--- Cancelacion_Historial_Reinicio/gestion_operaciones.py
+++ Cancelacion_Historial_Reinicio/gestion_operaciones.py
@@ -1,5 +1,14 @@
 import os
 import json
+from datetime import date
+
+mapa = [
+    {"producto": "hamburguesa", "cantidad": 2, "precio_unitario": 90},
+    {"producto": "papas", "cantidad": 1, "precio_unitario": 30},
+    {"producto": "refresco", "cantidad": 3, "precio_unitario": 15}
+]
+
+
 ARCHIVO_VENTAS = "ventas.txt"
 
 def verificar_archivo():
@@ -17,7 +26,8 @@ def agregar_pedido(cliente, orden, codigo):
     pedido = {
         "codigo": codigo,
         "cliente": cliente,
-        "pedido": orden
+        "pedido": orden,
+        "fecha": str(date.today())
     }
     with open(ARCHIVO_VENTAS, "a") as venta:
         venta.write(json.dumps(pedido) + "\n")
@@ -30,7 +40,8 @@ def gestionar_operaciones():
     if not verificar_archivo():
         crear_archivo()
     codigo = leer_archivo()
-    agregar_pedido("junior", {"hamburguesa": 180}, codigo)
+    agregar_pedido("junior", mapa,codigo)
+
 
 if __name__ == "__main__":
     gestionar_operaciones()
